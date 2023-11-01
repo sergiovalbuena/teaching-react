323bff1fcdecdd91c798f3705b142f38a7b3c015
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-10-31 22:27:45 -0700

    tic tac toc starter board and turns by state

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 
-import './App.css'
+const TURNS = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  return <div className={className}>{children}</div>;
+};
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
 
   return (
     <>
-<h1>Tic Tac Toe</h1>
+      <main className="board">
+        <h1>Tic Tac Toe</h1>
+        <section className="game">
+          {board.map((_, index) => {
+            return <Square key={index} index={index}></Square>;
+          })}
+        </section>
+
+        <div className="turn">
+          <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
