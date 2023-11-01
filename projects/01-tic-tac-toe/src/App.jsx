19960c46a2fa1555e3703fcb46d40d3eb7215f0d
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-10-31 22:40:07 -0700

    rendering x & o on the board

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -7,12 +7,30 @@ const TURNS = {
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
   const className = `square ${isSelected ? "is-selected" : ""}`;
-  return <div className={className}>{children}</div>;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
 };
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+  };
 
   return (
     <>
@@ -20,7 +38,11 @@ function App() {
         <h1>Tic Tac Toe</h1>
         <section className="game">
           {board.map((_, index) => {
-            return <Square key={index} index={index}></Square>;
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            );
           })}
         </section>
 
