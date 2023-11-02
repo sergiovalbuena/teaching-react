a88819c6fe6b1853a8ff22fb0eb36bfc436ad162
sergiovalbuena <iosergio.valbuena@gmail.com>
2023-11-02 13:42:31 -0700

    states for board, winner, 🧙🏼‍♂️ setWinner line 71 for sync state

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -19,17 +19,60 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   );
 };
 
+const WINNER_COMBOS = [
+  [0, 1, 2], //horizontal
+  [3, 4, 5], //horizontal
+  [6, 7, 8], //horizontal
+  [0, 3, 6], //vertical
+  [1, 4, 7], //vertical
+  [2, 5, 8], //vertical
+  [0, 4, 8], //diagonal
+  [2, 4, 6], //diagonal
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    //recorrer todas las combinaciones para ver si X u O gano
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] && //si no es null
+        boardToCheck[a] === boardToCheck[b] && //si son iguales
+        boardToCheck[a] === boardToCheck[c] //si son iguales
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  };
 
   const updateBoard = (index) => {
+    //no actualizamos la poscion si ya tieen algo
+    if (board[index]) {
+      return;
+    }
+    //actualizar tablero
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
-
+    //cambiar de turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
     setTurn(newTurn);
+
+    //revision de ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      // setWinner(newWinner);
+      // alert(`el ganador es ${newWinner}`);
+      setWinner((prev) => {
+        console.log(`Ganador: ${newWinner}, el anterior era ${prev}`);
+        return newWinner;
+      });
+    }
   };
 
   return (
